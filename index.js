4aa22c70928624d2cb2bcd1436d8042ed3a17113
Andy <tambe.ani@northeastern.edu>
2023-03-13 19:44:40 -0400

    configurable middleware component

--- index.js
+++ index.js
@@ -23,17 +23,29 @@ const app = connect();
 
 // logger middleware component - logs the request, calls next() function giving the control back to
 // the dispatcher.
-function logger(res,res,next){
-    console.log("New request received!");
-    next();
+
+// setup function - can be configured multiple times
+
+function logger(format){
+    const regexp = /:(\w+)/g;
+
+    return function createLogger(req,res,next){
+        const str = format.replace(regexp, (match,property) => {
+            return req[property];
+        });
+
+        console.log(str);
+        next();
+    }
 }
 
 // hello middeware component - ends the request, hence doesn't need next
 function hello(res,res){
+    res.setHeader('Content-Type','text/plain');
     res.end("Hello World!");
 }
 
 app
-.use(logger)
+.use(logger(':method :url'))
 .use(hello)
 .listen(3000);
