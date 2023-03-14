94775a11c11741df512c6b252c6653f1dcb23784
Andy <tambe.ani@northeastern.edu>
2023-03-13 20:29:51 -0400

    error handling middleware component added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const connect = require('connect');
 const app = connect();
-
+const env = process.env.NODE_ENV || 'development';
 /*
     use() - used for combining the middleware 
     components. 
@@ -45,7 +45,22 @@ function hello(res,res){
     res.end("Hello World!");
 }
 
+function errorHandler(err,req,res,next){
+    res.statusCode = 500;
+    switch(env){
+        case 'development':
+            console.error('Error:');
+            console.error(err);
+            res.setHeader('Content-Type');
+            res.end(JSON.stringify(err));
+            break;
+        default:
+            res.end('Server Error');
+    }
+}
+
 app
 .use(logger(':method :url'))
 .use(hello)
+.use(errorHandler)
 .listen(3000);
